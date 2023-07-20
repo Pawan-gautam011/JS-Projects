eb7a04c05f49e9153015afbb24ddb74d935f912c
Pawan-Gautam11 <pawangautam11op@gmail.com>
2023-07-20 23:54:55 +0545

    Completed this project

--- Dice ROll Game/script.js
+++ Dice ROll Game/script.js
@@ -1,18 +1,18 @@
 function shuffle(){
     const img= document.getElementById('img');
-    let random= Math.floor(Math.random()*6) + 1;
-    img.setAttribute("src", `assests/${random}.png`)
+    let random= Math.floor(Math.random() * 6) + 1;
+    img.setAttribute("src", `assests/${random}.png`  )
 };
 
-// function anim() {
-//     setTimeout(shuffle, 500);
-//     const img= document.getElementById('img');
-//     img.setAttribute("src", `assests/`)
+function anim() {
+    setTimeout(shuffle, 500);
+    const img= document.getElementById('img');
+    img.setAttribute("src", `assests/dice-rol.gif`)
 
 
 
     
-// }
+}
 
 
 
