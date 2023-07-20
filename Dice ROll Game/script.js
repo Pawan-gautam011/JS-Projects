ecb905de61a51318b883c877ba6e932b87a36bbf
Pawan-Gautam11 <pawangautam11op@gmail.com>
2023-07-20 23:45:50 +0545

    Almost completed this

---
+++ Dice ROll Game/script.js
@@ -0,0 +1,21 @@
+function shuffle(){
+    const img= document.getElementById('img');
+    let random= Math.floor(Math.random()*6) + 1;
+    img.setAttribute("src", `assests/${random}.png`)
+};
+
+// function anim() {
+//     setTimeout(shuffle, 500);
+//     const img= document.getElementById('img');
+//     img.setAttribute("src", `assests/`)
+
+
+
+    
+// }
+
+
+
+
+
+
