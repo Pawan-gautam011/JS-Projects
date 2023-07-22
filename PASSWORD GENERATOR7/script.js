4f0dcf8d2698cee4c64855bd38bc7134aee80e01
Pawan-Gautam11 <pawangautam11op@gmail.com>
2023-07-22 23:14:41 +0545

    Ready to make rotating navigation on my own understanding

--- PASSWORD GENERATOR7/script.js
+++ PASSWORD GENERATOR7/script.js
@@ -1,7 +1,26 @@
-const words=['BOB', 'BAM', 'BND', 'XOF', 'BIF', 'KHR',
-'XAF', 'CVE','CLP','COP','KMF','CRC','CUP','CDF','DJK','DOB',
-"apple", "banana","cherry","date",
-  "elderberry", "fig",
+const words = [
+  "BOB",
+  "BAM",
+  "BND",
+  "XOF",
+  "BIF",
+  "KHR",
+  "XAF",
+  "CVE",
+  "CLP",
+  "COP",
+  "KMF",
+  "CRC",
+  "CUP",
+  "CDF",
+  "DJK",
+  "DOB",
+  "apple",
+  "banana",
+  "cherry",
+  "date",
+  "elderberry",
+  "fig",
   "grape",
   "honeydew",
   "imbe",
@@ -20,26 +39,29 @@ const words=['BOB', 'BAM', 'BND', 'XOF', 'BIF', 'KHR',
   "vanilla",
   "watermelon",
   "xigua",
-  "yellow", "zucchini"
+  "yellow",
+  "zucchini",
+  "Kushal",
+  "LAmo",
+  "Sparsha",
 ];
-function getPassword(){
-    const chars='0123456789/%?@#$%';
-    const clength=chars.length;
-    let password='';
+function getPassword() {
+  var chars = "0123456789/%?@#$%abcdefghijklmnopqrstuvwxyz";
+  var clength = chars.length;
+  let password = "";
 
-    while(password.length<20){
-        password=password.concat(randomWord(chars.length));
-        password=password.concat(chars[randomNumber(clength)]);
-    }
-    password=password.substring(0,16);
-    document.getElementById('password').value=password;
+  while (password.length < 20) {
+    password = password.concat(randomWord(chars.length));
+    password = password.concat(chars[randomNumber(clength)]);
+  }
+  password = password.substring(0, 16);
+  document.getElementById("password").value = password;
 }
-function randomNumber(e){
-    return Math.floor(Math.random()* e);
-}
-
-function randomWord(){
-    const number=randomNumber(words.length);
-    return words[number]
+function randomNumber(e) {
+  return Math.floor(Math.random() * e);
 }
 
+function randomWord() {
+  const number = randomNumber(words.length);
+  return words[number];
+}
