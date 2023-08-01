e139754bb35254e3b07a615a56218460730545a3
Pawan-Gautam11 <pawangautam11op@gmail.com>
2023-08-01 21:43:26 +0545

    Search bar hiding project

---
+++ Hidden Search Widget/script.js
@@ -0,0 +1,24 @@
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click',()=>{
+    search.classList.toggle('active')
+    input.focus
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
