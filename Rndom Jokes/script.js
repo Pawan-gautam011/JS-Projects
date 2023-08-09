42110e39ba27543ffb01f58a8ebadc0aed21e1f0
Pawan-Gautam11 <pawangautam11op@gmail.com>
2023-08-09 08:19:32 +0545

    Added preloader

--- Rndom Jokes/script.js
+++ Rndom Jokes/script.js
@@ -1,21 +1,29 @@
-const jokeEl = document.getElementById('joke')
-const jokeBtn = document.getElementById('jokeBtn')
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
 
-jokeBtn.addEventListener('click', generateJoke)
+jokeBtn.addEventListener("click", generateJoke);
 
-generateJoke()
-
+generateJoke();
 
 async function generateJoke() {
   const Url = {
     headers: {
-      Accept: 'application/json',
+      Accept: "application/json",
     },
+  };
+
+  displayLoader();
+  const res = await fetch("https://icanhazdadjoke.com", Url);
+  hideLoader();
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
+
+  function displayLoader() {
+    document.querySelector(".lds-spinner").classList.add("active");
+    document.querySelector(".btn").classList.add("hide");
   }
-
-  const res = await fetch('https://icanhazdadjoke.com', Url)
-
-  const data = await res.json()
-
-  jokeEl.innerHTML = data.joke
+  function hideLoader() {
+    document.querySelector(".lds-spinner").classList.remove("active");
+    document.querySelector(".btn").classList.remove("hide");
+  }
 }
